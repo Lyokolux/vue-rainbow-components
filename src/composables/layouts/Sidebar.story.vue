4d2cb73dff4a5285d34dd71a1608125d9c93b062
Lyokolux <lyokolux@protonmail.com>
2023-01-11 21:25:21 +0100

    ✨(Sidebar.vue): add component with story

---
+++ src/composables/layouts/Sidebar.story.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+import Sidebar from './Sidebar.vue'
+</script>
+
+<template>
+  <Story title="Layouts/Sidebar">
+    <Variant title="Basic 100px">
+      <Sidebar class="width">
+        <ul>
+          <li>Item 1</li>
+          <li>Item 2</li>
+          <li>Item 3</li>
+          <li>Item 4</li>
+        </ul>
+      </Sidebar>
+    </Variant>
+    <Variant title="Full height">
+      <Sidebar class="full-height">
+        <ul>
+          <li>Item 1</li>
+          <li>Item 2</li>
+          <li>Item 3</li>
+          <li>Item 4</li>
+        </ul>
+      </Sidebar>
+    </Variant>
+  </Story>
+</template>
+
+<style lang="scss">
+@import '../../assets/colors.css';
+
+ul {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  box-sizing: border-box;
+
+  display: flex;
+  flex-flow: column nowrap;
+  gap: 1rem;
+}
+
+.full-height {
+  width: max(100px, 10vw);
+  height: 100vh;
+}
+
+.width {
+  width: max(10%, 100px);
+}
+</style>
---
+++ src/composables/layouts/Sidebar.vue
@@ -0,0 +1,15 @@
+<template>
+  <nav>
+    <slot />
+  </nav>
+</template>
+
+<style scoped lang="scss">
+nav {
+  box-sizing: border-box;
+  border-bottom-right-radius: .875rem;
+  border-top-right-radius: .875rem;
+  background-color: var(--c-main-background);
+  padding: 0.75rem 0.5rem 1rem 0.5rem;
+}
+</style>
